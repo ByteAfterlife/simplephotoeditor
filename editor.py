9ba1abba0b820ff8af2eceabaee635b170778f0a
Anth <113946747+anthgodz@users.noreply.github.com>
2024-03-10 12:42:10 -0400

    V2

--- editor.py
+++ editor.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageColor
+from PIL import Image, ImageColor, ImageEnhance, ImageFilter
 import customtkinter
 import os
 import sys
@@ -26,11 +26,14 @@ if checkifimg(path) == False:
     sys.exit()
 
 app = customtkinter.CTk()
-app.geometry("400x300")
+app.geometry("300x500")
 app.title("Adobe PS dollarama edition")
-
+# Defining default values
 RotationValue = 0
 Flip = ""
+Brightness = 0
+Sharpness = 0
+Emboss = 0
 def setrotate():
     global RotationValue
     RotationValue = customtkinter.CTkInputDialog(text="Value? ", title="Value Request")
@@ -57,7 +60,17 @@ def setflip():
     FlipXY = customtkinter.CTkButton(FlipApp, text="Flip both ways", command=flipboth)
     FlipXY.pack(padx=20, pady=30)
     FlipApp.mainloop()
-
+def setbrightness():
+    global Brightness
+    Brightness = customtkinter.CTkInputDialog(text="Value? ", title="Value Request")
+    Brightness = int(Brightness.get_input())
+def setsharpness():
+    global Sharpness
+    Sharpness = customtkinter.CTkInputDialog(text="Value? ", title="Value Request")
+    Sharpness = int(Sharpness.get_input())
+def emboss():
+    global Emboss
+    Emboss = 1
 
 
 
@@ -65,6 +78,12 @@ rotate = customtkinter.CTkButton(app, text="Rotate image", command=setrotate)
 rotate.pack(padx=20, pady=20)
 crop = customtkinter.CTkButton(app, text="Flip image", command=setflip)
 crop.pack(padx=20, pady=25)
+bright = customtkinter.CTkButton(app, text="Brighten image", command=setbrightness)
+bright.pack(padx=20, pady=30)
+sharp = customtkinter.CTkButton(app, text="Sharpen image", command=setsharpness)
+sharp.pack(padx=20, pady=35)
+emboss = customtkinter.CTkButton(app, text="Emboss image (not customizable)", command=emboss)
+emboss.pack(padx=20, pady=40)
 app.mainloop()
 
 
@@ -83,6 +102,12 @@ if os.path.exists(path):
         elif Flip == "B":
             image = image.transpose(Image.Transpose.FLIP_LEFT_RIGHT)
             image = image.transpose(Image.Transpose.FLIP_TOP_BOTTOM)
+    if Brightness != 0:
+        image = ImageEnhance.Brightness(image=image).enhance(Brightness)
+    if Sharpness != 0:
+        image = ImageEnhance.Sharpness(image=image).enhance(Sharpness)
+    if Emboss == 1:
+        image = image.filter(ImageFilter.EMBOSS)
     viewresult = customtkinter.CTk()
     viewresult.title("Final product")
     viewresult.geometry("350x200")
